465e7af5a990a4302a4c132c355b0f4b77b02c35
schaffne <huebli@gmail.com>
2014-06-17 11:18:04 +0200

    serving static files

--- spirit/settings.py
+++ spirit/settings.py
@@ -123,8 +123,11 @@ STATIC_ROOT = 'staticfiles'
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.join(PROJECT_PATH, 'static')
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
